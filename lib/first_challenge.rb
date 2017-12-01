78c69b44f03138e7a2aad613caa8c9ca7fc1db00
Mark Bello <mcbello51286@gmail.com>
2017-12-01 16:15:53 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,7 +20,7 @@ def first_challenge
       data.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
           value.each do |flavor|
-            flavor.delete_if(flavor == "strawberry")
+            value.delete_if(flavor == "strawberry")
           end
         end
       end
