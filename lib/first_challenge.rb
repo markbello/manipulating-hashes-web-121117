e65b787c1a731f7891fb5fea56689e01720bb5d5
Mark Bello <mcbello51286@gmail.com>
2017-12-01 15:45:18 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
   contacts.each do |person, data|
-    if key == "Freddy Mercury"
+    if person == "Freddy Mercury"
       data.each do |attribute, value|
         if attribute == "favorite_icecream_flavors"
           value.each do |flavor|
